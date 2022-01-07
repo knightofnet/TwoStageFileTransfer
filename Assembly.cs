59efa2d51b1afe02229e8a1febb054c3d15cc54a
Aryx <39461001+knightofnet@users.noreply.github.com>
2022-01-07 08:42:15 +0100

    Use AryxDevLibrary from nuget instead of local project (issue #2)

--- Assembly.cs
+++ Assembly.cs
@@ -6,6 +6,6 @@
 using System.Reflection;
 
 
-[assembly: AssemblyVersion("1.0.1.35")]
-[assembly: AssemblyFileVersion("1.0.1.35")]
+[assembly: AssemblyVersion("1.0.1.40")]
+[assembly: AssemblyFileVersion("1.0.1.40")]
 
