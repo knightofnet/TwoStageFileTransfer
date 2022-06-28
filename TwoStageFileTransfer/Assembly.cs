e59c66f6ff71facd52f46d707623c68ccbcd671c
Aryx <39461001+knightofnet@users.noreply.github.com>
2022-06-28 21:57:50 +0200

    Use of UsefulCsharpCommonsUtils as NugetPackage (not as side project)

--- TwoStageFileTransfer/Assembly.cs
+++ TwoStageFileTransfer/Assembly.cs
@@ -6,6 +6,6 @@
 using System.Reflection;
 
 
-[assembly: AssemblyVersion("1.0.1.211")]
-[assembly: AssemblyFileVersion("1.0.1.211")]
+[assembly: AssemblyVersion("1.0.1.212")]
+[assembly: AssemblyFileVersion("1.0.1.212")]
 
